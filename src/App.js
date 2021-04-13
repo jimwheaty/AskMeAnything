3e995b882c4bb4d868d92076a5cf4804cdeb626e
jimwheaty <jimstaras@gmail.com>
2021-04-13 13:01:07 +0300

    Trying a simple commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Jim Wheaty!!
+          Hi Jimmy!!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
