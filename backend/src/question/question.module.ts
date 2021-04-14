9aea486717b419956b4d552dda37446f5cd0f1a9
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-04-14 13:26:40 +0300

    Set up Question Module
    Question module is imported from root module (AppModule)

--- backend/src/question/question.module.ts
+++ backend/src/question/question.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Question } from './question.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Question])],
   controllers: [QuestionController],
   providers: [QuestionService]
 })
