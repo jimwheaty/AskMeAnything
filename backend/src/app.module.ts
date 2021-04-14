fc1f1350d7a40c450c19b914d29d1e7670022894
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-04-14 12:55:46 +0300

    Connect to mysql database
    Use sequelize ORM to connect to a remote database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+const db = require('../db.config.js');
 
+const sequelizeModule = SequelizeModule.forRoot({
+  host: db.HOST,
+  username: db.USERNAME,
+  password: db.PASSWORD,
+  database: db.DATABASE,
+  dialect: 'mysql',
+  autoLoadModels: true,
+  synchronize: true,
+  dialectOptions: {connectTimeout: 10000},
+  pool: { max: 5, min: 0, acquire: 30000, idle: 10000},
+  models: []
+})
 
 @Module({
-  imports: [],
+  imports: [sequelizeModule],
 })
 export class AppModule {}
