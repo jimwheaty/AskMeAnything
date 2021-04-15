95493ab8aff6114b2605d1cf99c3398b94b30e8f
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-04-15 19:58:20 +0300

    Create answer.service for serving answer-related requests

--- backend/src/answer/answer.service.ts
+++ backend/src/answer/answer.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAnswerDto } from './dto/create-answer.dto';
-import { UpdateAnswerDto } from './dto/update-answer.dto';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Answer } from './answer.model';
 
 @Injectable()
 export class AnswerService {
-  create(createAnswerDto: CreateAnswerDto) {
-    return 'This action adds a new answer';
+  constructor(
+    @InjectModel(Answer)
+    private answerModel: typeof Answer
+  ) {}
+
+  create(newAnswer: Answer): Promise<Answer> {
+    if (!newAnswer.body) {
+      throw new BadRequestException('Missing title of the answer');
+    }
+    return this.answerModel.create(newAnswer);
   }
 
-  findAll() {
-    return `This action returns all answer`;
+  findAll() : Promise<Answer[]> {
+    return this.answerModel.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} answer`;
+  async findOne(id: number): Promise<Answer> {
+    const answer = await this.answerModel.findByPk(id);
+    if (!answer) {
+      throw new NotFoundException(`Answer with id ${id} not found!`);
+    }
+    return answer;
   }
 
-  update(id: number, updateAnswerDto: UpdateAnswerDto) {
-    return `This action updates a #${id} answer`;
+  async update(id: number, answerUpdate: Answer): Promise<Answer> {
+    const answer = await this.findOne(id);
+    answer.body = answerUpdate.body? answerUpdate.body: answer.body;
+    answer.upVotes = answerUpdate.upVotes? answerUpdate.upVotes: answer.upVotes;
+    
+    await answer.save();
+    return answer;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} answer`;
+  async remove(id: number): Promise<void> {
+    const answer = await this.findOne(id);
+    await answer.destroy();
   }
 }
