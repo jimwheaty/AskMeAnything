00175d458c4a016142c70081eeca6db94d48b5e8
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-04-28 22:02:36 +0300

    Create jwt-strategy for authenticated-users-only endpoints

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt'
 
 
 @Injectable()
 export class AuthService {
 
     constructor(
-        private userService: UsersService
+        private userService: UsersService,
+        private jwtService: JwtService
     ) {}
 
     async validateUser(username: string, pass: string): Promise<any> {
@@ -14,6 +16,14 @@ export class AuthService {
         const { password, ...result } = user;
 
         return (user.password === pass)? result: null;
+    }
+
+    async login(user: any) {
+        const payload = { 
+            username: user.username, 
+            sub: user.id 
+        };
+        return { access_token: this.jwtService.sign(payload) };
     }
 
 }
