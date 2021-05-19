8e2377b7ff8c4d192b3896ac45237c5324c18258
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-05-19 14:03:00 +0300

    temp

--- backend/src/stats/stats.controller.ts
+++ backend/src/stats/stats.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { StatsService } from './stats.service';
 
 @Controller('stats')
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
+
+  @Get('/popular-tags')
+  getPopularTags(@Query('limit') limit: string) {
+    return this.statsService.getPopularTags(limit);
+  }
+
+  // // statistics of question with tags
+  // getNumberOfTags()
+
+  // get
+
+  // @Get('popular-tags/questions')
+  // getPopularQuestionsPerTag('limit')
 }
--- backend/src/stats/stats.service.ts
+++ backend/src/stats/stats.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { QueryTypes } from 'sequelize';
+import { Question } from 'src/question/question.model';
+import { Tag } from 'src/tags/tags.model';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+
+    constructor(
+        @InjectModel(Tag) 
+        private readonly tagModel: typeof Tag,
+
+        @InjectModel(Question) 
+        private readonly questionModel: typeof Question 
+    ) {}
+
+
+
+    async getPopularTags(limit: string): Promise<any>{
+
+        interface response {
+            count: number;
+        }
+
+        const popularTags: response = await this.tagModel.findAll({
+            raw: true,
+            attributes: [
+                'field',
+            ],
+            group: [
+                'field'
+            ]
+        });
+        if (!popularTags) return [];
+
+        const sortedTags = popularTags.sort((a,b) => b.count - a.count);
+
+        return popularTags;
+    }
+
+
+}
