a4c0af622b4561e02fccc4db08fc302333b4bdf9
jimwheaty <jimstaras@gmail.com>
2021-05-24 20:24:34 +0300

    connected "sign up" to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -766,47 +766,92 @@ function ActivityStatistics() {
     )
 }
 
-function Signup (props) {
-    return(
-        <Row className="justify-content-md-center" style={{marginBottom:30, marginTop:30}}>
-            <Col sm={4}>
-                <h2>Sign Up</h2>
-                <br />
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email" onChange={(e) => props.onChange(e)}/>
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+class Signup extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            success: false
+        }
+    }
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => props.onChange(e)}/>
-                    </Form.Group>
+    onSubmit() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username: this.props.username, password: this.props.password, email: this.props.email})
+        };
+        fetch('http://localhost:8080/api/users', requestOptions)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    if (result.error)
+                        this.setState({error : result.message})
+                    else
+                        this.setState({success : true})
+                }
+            )
+    }
+    render() {
+        const {error, success} = this.state;
+        return (
+            <Row className="justify-content-md-center" style={{marginBottom: 30, marginTop: 30}}>
+                <Col sm={4}>
+                    <h2>Sign Up</h2>
+                    <br/>
+                    <Form>
+                        <Form.Group controlId="formBasicUsername" >
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter username" name="username" onChange={(e) => this.props.onChange(e)}/>
+                        </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Re-enter Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" name="email" onChange={(e) => this.props.onChange(e)}/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-                    <ButtonGroup>
-                        <LinkContainer to="/">
-                            <Button variant="primary" type="button" name="isSigned" onClick={ (e) => props.onClick(e)}>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => this.props.onChange(e)}/>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Re-enter Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"/>
+                        </Form.Group>
+
+                        <ButtonGroup>
+                            <Button variant="primary" type="button" onClick={() => this.onSubmit()}>
                                 Sign Up
                             </Button>
-                        </LinkContainer>
-                        <LinkContainer to="/">
-                            <Button variant="secondary" type="submit">
-                                Cancel
-                            </Button>
-                        </LinkContainer>
-                    </ButtonGroup>
-                </Form>
-            </Col>
-        </Row>
-    );
+                            <LinkContainer to="/">
+                                <Button variant="secondary" type="submit">
+                                    Cancel
+                                </Button>
+                            </LinkContainer>
+                        </ButtonGroup>
+                    </Form>
+
+                    <Container>
+                        <br/>
+                        {error ?
+                            <Alert variant={'warning'}>{error}</Alert>
+                            :
+                            <Container/>
+                        }
+                        {success ?
+                            <Alert variant={'success'}>Thank you!</Alert>
+                            :
+                            <Container/>
+                        }
+                    </Container>
+                </Col>
+            </Row>
+        );
+    }
 }
 
 class Signin extends React.Component {
@@ -1363,8 +1408,11 @@ class App extends React.Component{
                         </Route>
                         <Route path="/sign_up">
                             <Signup
+                                username = {this.state.username}
+                                password = {this.state.password}
+                                email = {this.state.email}
                                 onChange={(event) => this.handleChange(event)}
-                                onClick={(event) => this.handleSignin(event)}
+                                onClick={(access_token) => this.handleSignin(access_token)}
                             />
                         </Route>
                         <Route path="/sign_in">
