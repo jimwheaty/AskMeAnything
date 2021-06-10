aa99383bb0e1483fe3e144fcaaff00a1f980da70
jimwheaty <jimstaras@gmail.com>
2021-06-10 20:04:53 +0300

    trying to understand deployment ... (try 13)

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'app.js'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
