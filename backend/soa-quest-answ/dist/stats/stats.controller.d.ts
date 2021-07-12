cbb423f052985550f0f65add382c4c75d1720da1
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-07-12 17:05:48 +0300

    ignore dist/ dir

--- backend/soa-quest-answ/dist/stats/stats.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { StatsService } from './stats.service';
-export declare class StatsController {
-    private readonly statsService;
-    constructor(statsService: StatsService);
-    getPopularTags(limit: string): Promise<import("../tags/tags.model").Tag[]>;
-    getQuestionsByDate(userId: string, year: string, month: string): Promise<import("../question/question.model").Question[]>;
-    getAnswersByDate(userId: string, year: string, month: string): Promise<import("../answer/answer.model").Answer[]>;
-}
--- backend/soa-quest-answ/dist/tags/dto/create-tag.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateTagDto = void 0;
-class CreateTagDto {
-}
-exports.CreateTagDto = CreateTagDto;
-//# sourceMappingURL=create-tag.dto.js.map
