04a23942c4fb40db6188b0fe6cf1b5ae9ad3f64c
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-07-13 19:52:21 +0300

    creare independent auth service v2

---
+++ backend/soa-auth/src/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./local.strategy";
+import { AuthController } from "./auth.controller";
+import constants from "./constants";
+import { JwtStrategy } from "./jwt.strategy";
+
+@Module({
+  imports: [
+    ClientsModule.register([{
+      name: 'USER_CLIENT',
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379'
+      }
+    }]),
+    // ClientsModule.register([{
+    //   name: 'USER_CLIENT',
+    //   transport: Transport.TCP,
+    //   options: {
+    //     host: '127.0.0.1',
+    //     port: 8080,
+    //   }
+    // }]),
+    JwtModule.register({
+      secret: constants.jwtSecret,
+      signOptions: { expiresIn: '600s' }
+  })],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy]
+})
+export class AuthModule {}
---
+++ backend/soa-auth/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  
+  const app = await NestFactory.create(AppModule);
+
+   app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+      url: 'redis://localhost:6379'
+    }
+  });
+
+  // app.connectMicroservice({
+  //   transport: Transport.TCP,
+  //   options: {
+  //     host: '127.0.0.1',
+  //     port: 3000,
+  //   }
+  // });
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+  console.log('Auth Service is running!');
+}
+bootstrap();
