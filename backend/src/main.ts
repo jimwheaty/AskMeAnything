e01e97b8a24de35412ac6d9c4cb1a8e691280aea
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-07-28 13:08:18 +0300

    minor changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
-  app.enableCors({
-    origin: 'http://localhost:3000'
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
 
-  const PORT = 8080;
-  await app.listen(process.env.PORT || 8080);
-  console.log(`App listening on port ${PORT}. Go to http://localhost:${PORT}`);
+  const PORT = process.env.PORT || 8080;
+  await app.listen(PORT);
+  console.log(`App listening!`);
 }
 bootstrap();
