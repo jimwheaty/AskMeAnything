95b6245d095c0a7e664849050defaf399070a07a
Eleftherios Kritsotakis <elkritsotakis@gmail.com>
2021-07-28 14:21:04 +0300

    minor changes

--- backend/soa-auth/src/auth/auth.module.ts
+++ backend/soa-auth/src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { AuthService } from "./auth.service";
 import { LocalStrategy } from "./local.strategy";
 import { AuthController } from "./auth.controller";
 import constants from "./constants";
-import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -16,14 +15,6 @@ import { JwtStrategy } from "./jwt.strategy";
         url: 'redis://localhost:6379'
       }
     }]),
-    // ClientsModule.register([{
-    //   name: 'USER_CLIENT',
-    //   transport: Transport.TCP,
-    //   options: {
-    //     host: '127.0.0.1',
-    //     port: 8080,
-    //   }
-    // }]),
     JwtModule.register({
       secret: constants.jwtSecret,
       signOptions: { expiresIn: '600s' }
